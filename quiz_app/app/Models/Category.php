fe79da1f6e45a0dd4377c2efeb6f18bed58a48cc
Hardik0131 <hardikbhaliya333@gmail.com>
2026-01-21 17:05:09 +0530

    Update the Category Model

--- quiz_app/app/Models/Category.php
+++ quiz_app/app/Models/Category.php
@@ -18,4 +18,8 @@ class Category extends Model
     public function posts(){
         return $this->hasMany(Post::class);
     }
+
+    public function questions(){
+        return $this->hasMany(Question::class);
+    }
 }
